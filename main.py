0b434b18dd2ab4b966769260304936ec52ec5896
jrodr117 <jrodr113@msudenver.edu>
2022-09-26 13:52:07 -0600

    converted data to list and dictionary

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import pandas
 
 data = pandas.read_csv("weather_data.csv")
 print(data)
+
+data_dictionary = data.to_dict()
+print(data_dictionary)
+
+temperature_list = data["temp"].to_list()
+print(temperature_list)
