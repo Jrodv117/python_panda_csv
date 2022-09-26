9c697075015052a93457b114e3a4ea8c99d4a4b0
jrodr117 <jrodr113@msudenver.edu>
2022-09-26 14:26:12 -0600

    changes some code to dot notation

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ data = pandas.read_csv("weather_data.csv")
 data_dictionary = data.to_dict()
 
 
-temperature_list = data["temp"].to_list()
+temperature_list = data.temp.to_list()
 
 
 def average_temperature():
-    average = data["temp"].mean()
+    average = data.temp.mean()
     print(average)
 
 
 def max_temperature():
-    maximum = data["temp"].max()
+    maximum = data.temp.max()
     print(maximum)
 
 
