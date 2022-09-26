caa35be54def9f9799445f92c634ae6253ce6b9e
jrodr117 <jrodr113@msudenver.edu>
2022-09-26 14:24:53 -0600

    function max_temperature_day finds the hottest day

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data)
+
 
 data_dictionary = data.to_dict()
 
@@ -11,16 +11,17 @@ temperature_list = data["temp"].to_list()
 
 def average_temperature():
     average = data["temp"].mean()
-    return average
+    print(average)
 
 
 def max_temperature():
-    max = data["temp"].max()
-    return max
+    maximum = data["temp"].max()
+    print(maximum)
 
 
-avg = average_temperature()
-max_temperature = max_temperature()
+def max_temperature_day():
+    hottest_day = data[data.temp == data.temp.max()].day
+    print(hottest_day)
 
-print(avg)
-print(max_temperature)
+
+max_temperature_day()
