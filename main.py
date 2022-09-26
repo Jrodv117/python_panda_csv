f0d9a3979dbf1dd43371224eabdc935557def4a8
jrodr117 <jrodr113@msudenver.edu>
2022-09-26 13:57:51 -0600

    made a function to calculate average temperature

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ data = pandas.read_csv("weather_data.csv")
 print(data)
 
 data_dictionary = data.to_dict()
-print(data_dictionary)
+
 
 temperature_list = data["temp"].to_list()
-print(temperature_list)
+
+
+def average_temperature():
+    total_temperature = 0
+    for i in temperature_list:
+        total_temperature += i
+    average = total_temperature / (len(temperature_list))
+    return average
+
+
+avg = average_temperature()
+print(avg)
